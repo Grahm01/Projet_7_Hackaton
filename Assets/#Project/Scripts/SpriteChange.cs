65a5c5203f68f6ef91c08f7f70d3ad83fc704b9f
A <urbaniak_agata@hotmail.com>
2021-12-09 11:57:47 +0100

    Test Scene Agata - sprite changer

--- Assets/#Project/Scripts/SpriteChange.cs
+++ Assets/#Project/Scripts/SpriteChange.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SpriteChange : MonoBehaviour
 {
-    
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
+
+    public enum SpriteStatus { open1 = 0, closed1 = 1}
+    public SpriteStatus spriteState;
+
     void Start()
     {
-        
+        OnMouseClick();
     }
-
-    
     void Update()
     {
+        spriteRenderer.sprite = spriteArray[(int)spriteState];
         
+
     }
+    public void OnMouseClick()
+    {
+            spriteState = SpriteStatus.closed1;
+
+    }
+    
 }
