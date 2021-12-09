9f126847de3c3eea93c623980966f14925c348ed
A <urbaniak_agata@hotmail.com>
2021-12-09 13:35:40 +0100

    Buttons OK - WIP Agata Scene

--- Assets/#Project/Scripts/SpriteChange.cs
+++ Assets/#Project/Scripts/SpriteChange.cs
@@ -6,6 +6,7 @@ public class SpriteChange : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite[] spriteArray;
+    private int rando;
 
     public enum SpriteStatus { open1 = 0, closed1 = 1}
     public SpriteStatus spriteState;
@@ -16,6 +17,7 @@ public class SpriteChange : MonoBehaviour
     }
     void Update()
     {
+        rando = Random.Range(0, 1);
         spriteRenderer.sprite = spriteArray[(int)spriteState];
         
 
